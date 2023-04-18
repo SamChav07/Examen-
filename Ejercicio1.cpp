09b64c91ed185a7de11d0ecff5088cc1acaa5230
rjsarrias <rjsarrias@uamv.edu.ni>
2023-04-18 10:59:36 -0600

    Ejercicio 1

---
+++ Ejercicio1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int cant;
+  float precio, subtotal, descuento, impuesto, total;
+
+  cout << "Ingrese el precio del artículo: ";
+  cin >> precio;
+
+  cout << "Ingrese la cantidad de articulos que desea comprar: ";
+  cin >> cant;
+
+
+  subtotal = cant * precio;
+
+  if (cant >= 2) {
+    descuento = subtotal * 0.2;
+  } else {
+    descuento = 0;
+  }
+
+  impuesto = subtotal * 0.15;
+  total = subtotal - descuento + impuesto;
+
+  cout << "Subtotal: C$" << subtotal << endl;
+  cout << "Descuento: C$" << descuento << endl;
+  cout << "Impuesto: C$" << impuesto << endl;
+  cout << "Total a pagar: C$" << total << endl;
+
+  return 0;
+}
