2871a158dda274a66c51d0f1f3e00d6d6942e11a
Gabi0808 <giperez@uamv.edu.ni>
2023-04-18 11:14:52 -0600

    Este es mi aporte al trabajo

---
+++ Gabriel.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void ejercicio2();
+
+int main() {
+    const double PRECIO_SENCILLA = 50.00;
+    const double PRECIO_DOBLE = 75.00;
+    const double PRECIO_TRIPLE = 100.00;
+    const double CARGO_TARJETA = 0.05; 
+
+    int cantidad_sencillas, cantidad_dobles, cantidad_triples;
+    double costo_total, costo_con_cargo;
+
+    
+    cout << "Ingrese la cantidad de hamburguesas sencillas que desea comprar: ";
+    cin >> cantidad_sencillas;
+    cout << "Ingrese la cantidad de hamburguesas dobles que desea comprar: ";
+    cin >> cantidad_dobles;
+    cout << "Ingrese la cantidad de hamburguesas triples que desea comprar: ";
+    cin >> cantidad_triples;
+   
+    costo_total = cantidad_sencillas * PRECIO_SENCILLA + cantidad_dobles * PRECIO_DOBLE + cantidad_triples * PRECIO_TRIPLE;
+
+    
+    costo_con_cargo = costo_total * (1 + CARGO_TARJETA);
+
+    
+    cout << "El costo total de las hamburguesas es: C$" << costo_total << endl;
+    cout << "Si paga con tarjeta, se le cargara un 5% adicional. El total a pagar es: C$" << costo_con_cargo << endl;
+
+    return 0;
+    
+}
