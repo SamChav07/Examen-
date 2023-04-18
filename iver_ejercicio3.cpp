715340cf4f90c6af13705c5152282a5b24ab3718
IverDowns <127228835+IverDowns@users.noreply.github.com>
2023-04-18 11:29:43 -0600

    Ejercicio3
    Ejercicio3

---
+++ iver_ejercicio3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numLlamadas;
+    double bono;
+    
+    cout << "Ingrese el número de llamadas atendidas en la semana: ";
+    cin >> numLlamadas;
+    
+    if (numLlamadas > 50 && numLlamadas < 100) {
+        bono = 1000.00;
+    } else if (numLlamadas > 100 && numLlamadas < 150) {
+        bono = 1500.00;
+    } else if (numLlamadas > 150) {
+        bono = 2000.00;
+    } else {
+        bono = 0.00;
+    }
+    
+    if (bono > 0.00) {
+        cout << "El empleado recibe un bono de C$" << bono << endl;
+    } else {
+        cout << "El empleado no cumple con las condiciones para recibir un bono." << endl;
+    }
+    
+    return 0;
+}
